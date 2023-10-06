c3b7ab88c18f978bea7fbbd8b6d593f6a44da3d1
darkkillex <mastrangelo.nicola@gmail.com>
2023-10-06 20:55:25 +0200

    Added exception handling to the loading of source file

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import logging
 from openpyxl import Workbook
 from openpyxl.styles import Font
 from openpyxl.utils.dataframe import dataframe_to_rows
@@ -11,8 +12,18 @@ wb_adr, wb_not_adr = Workbook(), Workbook()
 
 
 def load_xlsx_file(file_excel):
-    #load a xlsx file in a DF
-    df = pd.read_excel(file_excel)
+    df = None
+    try:
+        df = pd.read_excel(file_excel)
+        # Operazioni sul dataframe df qui
+    except FileNotFoundError:
+        print("Il file Excel non è stato trovato.")
+    except pd.errors.EmptyDataError:
+        print("Il file Excel è vuoto o non contiene dati.")
+    except pd.errors.ParserError:
+        print("Errore durante la lettura del file Excel. Assicurati che il formato sia corretto.")
+    except Exception as e:
+        print(f"Si è verificato un errore sconosciuto: {str(e)}")
     return df
 
 
@@ -44,11 +55,12 @@ def create_IN_OUT_report(wb, sheet, label):
 def run_scripts():
     # let's go work on In_OUT DFs
     df_1 = load_xlsx_file(constants.IN_OUT)
-    df_final = clean_df(df_1)
-    df = inOut.create_data_struct(df_final)
-    df = inOut.populate_sheets(df)
-    create_IN_OUT_report(wb_adr, inOut.adr_sheet, "assets/IN_OUT_ADR.xlsx")
-    create_IN_OUT_report(wb_not_adr, inOut.not_adr_sheet, "assets/IN_OUT_NON_ADR.xlsx")
+    if df_1 is not None:
+        df_final = clean_df(df_1)
+        df = inOut.create_data_struct(df_final)
+        df = inOut.populate_sheets(df)
+        create_IN_OUT_report(wb_adr, inOut.adr_sheet, "assets/IN_OUT_ADR.xlsx")
+        create_IN_OUT_report(wb_not_adr, inOut.not_adr_sheet, "assets/IN_OUT_NON_ADR.xlsx")
 
 
 if __name__ == '__main__':
