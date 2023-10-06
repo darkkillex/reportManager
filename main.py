5c1c41b109c6296556e5b354e576c65d4006a584
darkkillex <mastrangelo.nicola@gmail.com>
2023-10-06 17:38:10 +0200

    Added load xlsx file function

--- main.py
+++ main.py
@@ -2,15 +2,20 @@
 
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pandas as pd
+import constants
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_xlsx_file(file_excel):
+    #load a xlsx file in a DF
+    df = pd.read_excel(file_excel)
+    return df
 
 
-# Press the green button in the gutter to run the script.
+
+
+
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
