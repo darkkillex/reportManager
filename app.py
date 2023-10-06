6cdb4942195af04eef52a17f7b7832df6843bb15
darkkillex <mastrangelo.nicola@gmail.com>
2023-10-06 21:44:11 +0200

    Added TODO for getting the common date to add to label of report Excel

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from datetime import datetime
 import logging
 from openpyxl import Workbook
 from openpyxl.styles import Font
@@ -52,10 +53,37 @@ def create_IN_OUT_report(wb, sheet, label):
     wb.save(label)
 
 
+# TODO get the common date to add to label of report Excel
+def generate_report_date(df):
+    # Estrai la prima colonna contenente le date come stringhe
+    date_column = df.iloc[:, 0]
+
+    # Inizializza una lista vuota per conservare le date valide
+    date_objects = []
+
+    # Converte le stringhe in oggetti datetime, gestendo gli errori
+    for date_string in date_column:
+        try:
+            date_time_obj = datetime.strptime(date_string, "%d/%m/%Y")
+            date_objects.append(date_time_obj)
+        except ValueError:
+            # Se la conversione va in errore, puoi decidere come gestire la data non valida
+            # In questo esempio, semplicemente aggiungiamo None alla lista
+            date_objects.append(None)
+
+    # Conta le occorrenze di ciascuna data
+    date_counts = date_column.value_counts()
+    # Trova la data con il conteggio massimo (data comune)
+    common_date = date_counts.idxmax()
+    return common_date
+
+
 def run_scripts():
     # let's go work on In_OUT DFs
     df_1 = load_xlsx_file(constants.IN_OUT)
     if df_1 is not None:
+        print(df_1)
+        print(generate_report_date(df_1))
         df_final = clean_df(df_1)
         df = inOut.create_data_struct(df_final)
         df = inOut.populate_sheets(df)
@@ -65,4 +93,5 @@ def run_scripts():
 
 if __name__ == '__main__':
     run_scripts()
+    print("IN_OUT_ADR.xlsx and IN_OUT_NON_ADR.xlsx has been generated!")
 
