22ebc58a813f335d4e0511449fea882be00f2875
darkkillex <mastrangelo.nicola@gmail.com>
2023-10-08 15:50:33 +0200

    Creation of a menù for choosing the desired functionality

--- app.py
+++ app.py
@@ -1,44 +1,28 @@
-import pandas as pd
-from datetime import datetime
-import logging
-from openpyxl import Workbook
-from openpyxl.styles import Font
-from openpyxl.utils.dataframe import dataframe_to_rows
-
 import constants
 import utilities
 import in_out_vob as iov
 
 
-def run_scripts():
-    # let's go work on DFs
-    df_in_out = iov.load_xlsx_file(constants.IN_OUT)
-    df_vob_pob = iov.load_xlsx_file(constants.VOB_POB)
+def make_your_choice():
+    print("##################################################")
+    print("##      Select an option:                        #")
+    print("###      1.Report IN_OUT and VOB               ##")
+    print("##################################################")
 
-    if df_in_out is not None:
-        df_final_in_out = iov.clean_df(df_in_out)
-        df_ultimate_in_out = utilities.create_df_data_struct(df_final_in_out, type_of_report=constants.REPORT_IN_OUT)
-        utilities.populate_sheets(df_ultimate_in_out,iov.adr_sheet_in_out, iov.not_adr_sheet_in_out)
-        iov.create_report(iov.wb_adr_in_out, iov.adr_sheet_in_out,
-                      iov.generate_report_label(df_ultimate_in_out, constants.LABEL_REPORT_IN_OUT_ADR))
-        iov.create_report(iov.wb_not_adr_in_out, iov.not_adr_sheet_in_out, iov.generate_report_label(df_ultimate_in_out,
-                                                                                     constants.LABEL_REPORT_IN_OUT_NOT_ADR))
-    if df_vob_pob is not None:
-        df_final_vob_pob = iov.clean_df(df_vob_pob)
-        df_ultimate_vob_pob = utilities.create_df_data_struct(df_final_vob_pob, type_of_report=constants.REPORT_VOB_POB)
-        utilities.populate_sheets(df_ultimate_vob_pob, iov.adr_sheet_vob_pob, iov.not_adr_sheet_vob_pob)
-        #print(df_ultimate_vob_pob.head())
-        iov.create_report(iov.wb_adr_vob_pob, iov.adr_sheet_vob_pob, iov.generate_report_label(df_ultimate_in_out,#in this case we get the df_in_out to get the right date of extraction
-                                                                               constants.LABEL_REPORT_VOB_POB_ADR))
-        iov.create_report(iov.wb_not_adr_vob_pob, iov.not_adr_sheet_vob_pob, iov.generate_report_label(df_ultimate_in_out,#in this case we get the df_in_out to get the right date of extraction
-                                                                                       constants.LABEL_REPORT_VOB_POB_NOT_ADR))
+    while True:
+        choice = input("Please enter you choice: ")
+
+        if choice == "1":
+            iov.run_scripts()
+            print("IN_OUT and VOB reports have been generated in the /assets folder!")
+            break
+        else:
+            print("Unavailable choice. Please enter a valid choice")
 
 
 if __name__ == '__main__':
     try:
-        run_scripts()
+        make_your_choice()
     except Exception as e:
         print(f"Si è verificato un errore: {str(e)}")
-    else:
-        print("IN_OUT and VOB reports have been generated in the /assets folder!")
 
--- in_out_vob.py
+++ in_out_vob.py
@@ -79,35 +79,26 @@ def generate_report_label(df, label):
     return report_label
 
 
-# def run_scripts():
-#     # let's go work on DFs
-#     df_in_out = load_xlsx_file(constants.IN_OUT)
-#     df_vob_pob = load_xlsx_file(constants.VOB_POB)
-#
-#     if df_in_out is not None:
-#         df_final_in_out = clean_df(df_in_out)
-#         df_ultimate_in_out = utilities.create_df_data_struct(df_final_in_out, type_of_report=constants.REPORT_IN_OUT)
-#         utilities.populate_sheets(df_ultimate_in_out,adr_sheet_in_out, not_adr_sheet_in_out)
-#         create_report(wb_adr_in_out, adr_sheet_in_out,
-#                       generate_report_label(df_ultimate_in_out, constants.LABEL_REPORT_IN_OUT_ADR))
-#         create_report(wb_not_adr_in_out, not_adr_sheet_in_out, generate_report_label(df_ultimate_in_out,
-#                                                                                      constants.LABEL_REPORT_IN_OUT_NOT_ADR))
-#     if df_vob_pob is not None:
-#         df_final_vob_pob = clean_df(df_vob_pob)
-#         df_ultimate_vob_pob = utilities.create_df_data_struct(df_final_vob_pob, type_of_report=constants.REPORT_VOB_POB)
-#         utilities.populate_sheets(df_ultimate_vob_pob, adr_sheet_vob_pob, not_adr_sheet_vob_pob)
-#         #print(df_ultimate_vob_pob.head())
-#         create_report(wb_adr_vob_pob, adr_sheet_vob_pob, generate_report_label(df_ultimate_in_out,#in this case we get the df_in_out to get the right date of extraction
-#                                                                                constants.LABEL_REPORT_VOB_POB_ADR))
-#         create_report(wb_not_adr_vob_pob, not_adr_sheet_vob_pob, generate_report_label(df_ultimate_in_out,#in this case we get the df_in_out to get the right date of extraction
-#                                                                                        constants.LABEL_REPORT_VOB_POB_NOT_ADR))
-#
-#
-# if __name__ == '__main__':
-#     try:
-#         run_scripts()
-#     except Exception as e:
-#         print(f"Si è verificato un errore: {str(e)}")
-#     else:
-#         print("IN_OUT and VOB reports have been generated in the /assets folder!")
+def run_scripts():
+    # let's go work on DFs
+    df_in_out = load_xlsx_file(constants.IN_OUT)
+    df_vob_pob = load_xlsx_file(constants.VOB_POB)
+
+    if df_in_out is not None:
+        df_final_in_out = clean_df(df_in_out)
+        df_ultimate_in_out = utilities.create_df_data_struct(df_final_in_out, type_of_report=constants.REPORT_IN_OUT)
+        utilities.populate_sheets(df_ultimate_in_out,adr_sheet_in_out, not_adr_sheet_in_out)
+        create_report(wb_adr_in_out, adr_sheet_in_out,
+                      generate_report_label(df_ultimate_in_out, constants.LABEL_REPORT_IN_OUT_ADR))
+        create_report(wb_not_adr_in_out, not_adr_sheet_in_out, generate_report_label(df_ultimate_in_out,
+                                                                                     constants.LABEL_REPORT_IN_OUT_NOT_ADR))
+    if df_vob_pob is not None:
+        df_final_vob_pob = clean_df(df_vob_pob)
+        df_ultimate_vob_pob = utilities.create_df_data_struct(df_final_vob_pob, type_of_report=constants.REPORT_VOB_POB)
+        utilities.populate_sheets(df_ultimate_vob_pob, adr_sheet_vob_pob, not_adr_sheet_vob_pob)
+        #print(df_ultimate_vob_pob.head())
+        create_report(wb_adr_vob_pob, adr_sheet_vob_pob, generate_report_label(df_ultimate_in_out,#in this case we get the df_in_out to get the right date of extraction
+                                                                               constants.LABEL_REPORT_VOB_POB_ADR))
+        create_report(wb_not_adr_vob_pob, not_adr_sheet_vob_pob, generate_report_label(df_ultimate_in_out,#in this case we get the df_in_out to get the right date of extraction
+                                                                                       constants.LABEL_REPORT_VOB_POB_NOT_ADR))
 
